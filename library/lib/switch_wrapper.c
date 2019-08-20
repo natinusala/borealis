d947d839bcf32a6c0e0fa623c7c88c3c4ea0f2ff
WerWolv <werwolv98@gmail.com>
2019-08-20 17:25:22 +0200

    Removed memory hack for the swkbd as hbloader now supports it directly

--- library/lib/switch_wrapper.c
+++ library/lib/switch_wrapper.c
@@ -2,18 +2,9 @@
 #include <unistd.h>
 
 static int nxlink_sock = -1;
-static void *haddr;
-extern char *fake_heap_end;
-extern u32 __nx_applet_type;
 
 void userAppInit()
 {
-    if (__nx_applet_type != AppletType_Application && __nx_applet_type != AppletType_SystemApplication) {
-        // Setup Heap for swkbd on applets
-        svcSetHeapSize(&haddr, 0x10000000);
-        fake_heap_end = (char*) haddr + 0x10000000;
-    }
-
     romfsInit();
     socketInitializeDefault();
     nxlink_sock = nxlinkStdio();
@@ -23,9 +14,6 @@ void userAppInit()
 
 void userAppExit()
 {
-    if (__nx_applet_type != AppletType_Application && __nx_applet_type != AppletType_SystemApplication)
-        svcSetHeapSize(&haddr, ((u8*) envGetHeapOverrideAddr() + envGetHeapOverrideSize()) - (u8*) haddr); // clean up the heap
-
     if (nxlink_sock != -1)
         close(nxlink_sock);
     socketExit();
