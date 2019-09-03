22b28f715bc3da7fb844fc174bb9c44c1b4618ce
WerWolv <werwolv98@gmail.com>
2019-09-03 11:48:05 +0200

    Added layer view

---
+++ library/include/borealis/layer_view.hpp
@@ -0,0 +1,48 @@
+/*
+    Borealis, a Nintendo Switch UI Library
+    Copyright (C) 2019  WerWolv
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <vector>
+
+#include <borealis/view.hpp>
+
+namespace brls
+{
+
+class LayerView : public View
+{
+public:
+    LayerView();
+    ~LayerView();
+
+    void addLayer(View *view);
+    void changeLayer(int index, bool focus = true);
+    int getLayerIndex();
+
+    View* requestFocus(FocusDirection direction, View *oldFocus, bool fromUp);
+
+    void draw(NVGcontext *vg, int x, int y, unsigned width, unsigned height, Style *style, FrameContext *ctx) override;
+    void layout(NVGcontext* vg, Style *style, FontStash *stash) override;
+
+private:
+    std::vector<View *> layers;
+    int selectedIndex = 0;
+};
+
+}
