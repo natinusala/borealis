d779a48949928f8f3efd5a289b544cbea2a759db
WerWolv <werwolv98@gmail.com>
2019-09-19 14:48:34 +0200

    Redo frame rate independent repeating button delays

--- library/lib/application.cpp
+++ library/lib/application.cpp
@@ -56,6 +56,8 @@ constexpr uint32_t WINDOW_HEIGHT   = 720;
 constexpr const char* WINDOW_TITLE = WINDOW_NAME;
 
 #define DEFAULT_FPS 60
+#define BUTTON_REPEAT_DELAY 15
+#define BUTTON_REPEAT_CADENCY 5
 
 // glfw code from the glfw hybrid app by fincs
 // https://github.com/fincs/hybrid_app
@@ -302,8 +304,6 @@ bool Application::init(Style style, Theme theme)
 
 bool Application::mainLoop()
 {
-    static int repeatingButtonTimer = 0;
-
     // Frame start
     retro_time_t frameStart = 0;
     if (Application::frameTime > 0.0f)
@@ -342,26 +342,32 @@ bool Application::mainLoop()
 
     // Trigger gamepad events
     // TODO: Translate axis events to dpad events here
-    // TODO: Handle key repetition
+
     bool anyButtonPressed = false;
     bool repeating        = false;
+    static int repeatingButtonTimer = 0;
+    static int buttonPressTime = 0;
+
     for (int i = GLFW_GAMEPAD_BUTTON_A; i <= GLFW_GAMEPAD_BUTTON_LAST; i++)
     {
         if (Application::gamepad.buttons[i] == GLFW_PRESS)
         {
             anyButtonPressed = true;
-            repeating        = (repeatingButtonTimer > static_cast<int>((1000.0F / Application::frameTime) / 4.0F) && repeatingButtonTimer % static_cast<int>((1000.0F / Application::frameTime) / 8.0F) == 0);
+            repeating        = (repeatingButtonTimer > BUTTON_REPEAT_DELAY && repeatingButtonTimer % BUTTON_REPEAT_CADENCY == 0);
 
             if (Application::oldGamepad.buttons[i] != GLFW_PRESS || repeating)
                 Application::onGamepadButtonPressed(i, repeating);
         }
 
         if (Application::gamepad.buttons[i] != Application::oldGamepad.buttons[i])
-            repeatingButtonTimer = 0;
+            buttonPressTime = repeatingButtonTimer = 0;
     }
 
-    if (anyButtonPressed)
+    if (anyButtonPressed && cpu_features_get_time_usec() - buttonPressTime > 1000)
+    {
+        buttonPressTime = cpu_features_get_time_usec();
         repeatingButtonTimer++;
+    }
 
     Application::oldGamepad = Application::gamepad;
 
