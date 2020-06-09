3e154e5b9360d56a321564af9f26bb46d48b61c3
HookedBehemoth <Luisscheurenbrand@gmail.com>
2020-06-10 01:02:59 +0200

    cleanup and add credits to class name demangling

--- library/include/borealis/view.hpp
+++ library/include/borealis/view.hpp
@@ -207,10 +207,10 @@ class View
 
     std::string describe() const
     {
+        // Taken from: https://stackoverflow.com/questions/281818/unmangling-the-result-of-stdtype-infoname/4541470#4541470
         const char* name = typeid(*this).name();
-        int status       = -4; // some arbitrary value to eliminate the compiler warning
+        int status       = 0;
 
-        // enable c++11 by passing the flag -std=c++11 to g++
         std::unique_ptr<char, void (*)(void*)> res {
             abi::__cxa_demangle(name, NULL, NULL, &status),
             std::free
