bc98df3e5c3d7d36ed78600ae76a7f4446ca7e1a
Chris Bradel <ChrisBradel14@gmail.com>
2020-08-15 02:46:25 -0400

    remove band-aid thanks to fundamental fix in pr #54

--- library/lib/application.cpp
+++ library/lib/application.cpp
@@ -383,33 +383,29 @@ bool Application::mainLoop()
     //Make sure we got all our expected sticks before reading from them
     if (count >= 4)
     {
+        // Left Stick X
+        if (axes[0] < -.5)
+            Application::gamepad.buttons[GLFW_GAMEPAD_BUTTON_DPAD_LEFT] = GLFW_PRESS;
+        else if (axes[0] > .5)
+            Application::gamepad.buttons[GLFW_GAMEPAD_BUTTON_DPAD_RIGHT] = GLFW_PRESS;
+
         // Left Stick Y
         if (axes[1] < -.5)
             Application::gamepad.buttons[GLFW_GAMEPAD_BUTTON_DPAD_UP] = GLFW_PRESS;
         else if (axes[1] > .5)
             Application::gamepad.buttons[GLFW_GAMEPAD_BUTTON_DPAD_DOWN] = GLFW_PRESS;
-        else
-        {
-            // Left Stick X
-            if (axes[0] < -.5)
-                Application::gamepad.buttons[GLFW_GAMEPAD_BUTTON_DPAD_LEFT] = GLFW_PRESS;
-            else if (axes[0] > .5)
-                Application::gamepad.buttons[GLFW_GAMEPAD_BUTTON_DPAD_RIGHT] = GLFW_PRESS;
-        }
+
+        // Right Stick X
+        if (axes[2] < -.5)
+            Application::gamepad.buttons[GLFW_GAMEPAD_BUTTON_DPAD_LEFT] = GLFW_PRESS;
+        else if (axes[2] > .5)
+            Application::gamepad.buttons[GLFW_GAMEPAD_BUTTON_DPAD_RIGHT] = GLFW_PRESS;
 
         // Right Stick Y
         if (axes[3] < -.5)
             Application::gamepad.buttons[GLFW_GAMEPAD_BUTTON_DPAD_UP] = GLFW_PRESS;
         else if (axes[3] > .5)
             Application::gamepad.buttons[GLFW_GAMEPAD_BUTTON_DPAD_DOWN] = GLFW_PRESS;
-        else
-        {
-            // Right Stick X
-            if (axes[2] < -.5)
-                Application::gamepad.buttons[GLFW_GAMEPAD_BUTTON_DPAD_LEFT] = GLFW_PRESS;
-            else if (axes[2] > .5)
-                Application::gamepad.buttons[GLFW_GAMEPAD_BUTTON_DPAD_RIGHT] = GLFW_PRESS;
-        }
     }
 
     bool anyButtonPressed               = false;
