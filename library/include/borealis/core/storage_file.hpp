1e3a038d57b9e8da94559dfa5524a3ca56f806de
EmreTech <emreterzioglu49@gmail.com>
2021-03-09 15:25:48 -0800

    Added small examples to the documentation comments

--- library/include/borealis/core/storage_file.hpp
+++ library/include/borealis/core/storage_file.hpp
@@ -21,13 +21,50 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <borealis/core/logger.hpp>
+#include <borealis/core/util.hpp>
 #include <borealis/core/i18n.hpp>
 
 using namespace brls::literals;
+using namespace tinyxml2;
 
 namespace brls 
 {
+
+XMLElement *getElementByName(XMLDocument &ele, std::string const &elemt_value)
+{
+    // Code taken from: https://stackoverflow.com/questions/11921463/find-a-specific-node-in-a-xml-document-with-tinyxml/60964820#60964820
+    XMLElement *elem = ele.FirstChildElement(); //Tree root
+
+    while (elem)
+    {
+        if (!std::string(elem->Value()).compare(elemt_value))
+            return elem;
+        if (elem->FirstChildElement())
+        {
+            elem = elem->FirstChildElement();
+        }
+        else if (elem->NextSiblingElement())
+        {
+            elem = elem->NextSiblingElement();
+        }
+        else
+        {
+            if (elem->Parent()->ToElement()->NextSiblingElement())
+            {
+                elem = elem->Parent()->ToElement()->NextSiblingElement();
+            }
+            else if (elem->Parent()->ToElement()->FirstChildElement()
+                        &&  strcmp(elem->Name(), elem->Parent()->ToElement()->FirstChildElement()->Name()))
+            {
+                elem = elem->Parent()->ToElement()->FirstChildElement();
+            }
+            else {
+                break;
+            }
+        }
+    }
+    return NULL;
+}
 
 #define BRLS_STORAGE_FILE_INIT(filename) StorageFile::init(filename)
 
@@ -61,30 +98,49 @@ bool init(std::string filename) {
  * An example would be the following:
  * 
  * <brls:StorageFile>
- *      <brls:BoolProperty name="wizard_shown" value="true" />
+ *      <brls:Property name="wizard_shown" value="true" />
  *	
- *	    <brls:BoolProperty name="username" value="h4ck3rm4n" />
+ *	    <brls:Property name="username" value="h4ck3rm4n" />
  *	
- *	    <brls:StringListProperty name="favorites">
+ *	    <brls:ListProperty name="favorites">
  *		    <brls:Value value="borealis" />
- *	    </brls:StringListProperty>
+ *	    </brls:ListProperty>
  * </brls>
+ * 
+ * It gives a fatal error if the filename is not found (call the init function before these functions)
  */
 bool writeToFile(T value, std::string name) {
-    tinyxml2::XMLDocument file;
+    if (!std::filesystem::exists(config_folder + filename)) {
+        fatal("File {} not found in {}. ", filename, config_folder);
+        return false;
+    }
+
+    XMLDocument file;
     file.LoadFile(filename.c_str());
+
+    
 
     return true;
 }
 
 /*
  * Reads a value from the config file, then returns a variable pointing to that value.
+ * 
+ * For example, if you store a variable with the data "EmreTech is awesome" into a storage file,
+ * This function will find that value and return it, so you can read/change the value throughout
+ * the program running
  */
 T readToFile(std::string name) {
-    std::fstream file;
-    file.open(filename);
-    // TODO: Actually read data from an XML form into a normal variable
-    file.close();
+    if (!std::filesystem::exists(config_folder + filename)) {
+        fatal("File {} not found in {}. ", filename, config_folder);
+        return false;
+    }
+
+    XMLDocument file;
+    file.LoadFile(filename.c_str());
+
+    auto elementToFind = getElementByName(file, "brls:Property");
+
 }
 
 private:
