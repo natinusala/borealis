2a431f678c8ddd541b71229ed38a4f07c2325e36
EmreTech <emreterzioglu49@gmail.com>
2021-03-09 12:59:41 -0800

    Added work-in-progress StorageFile class
    See #140 in the upstream repo for more information.

---
+++ library/include/borealis/core/storage_file.hpp
@@ -0,0 +1,37 @@
+/*
+    Copyright 2021 EmreTech
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#pragma once
+
+#include <nlohmann/json.hpp>
+#include <tinyxml2/tinyxml2.h>
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <borealis/core/logger.hpp>
+
+namespace brls 
+{
+
+// The StorageFile class allows you to create, write, or read from files for anything
+// For example, you can use it for a config or track when a user last used the app
+class StorageFile
+{
+
+};
+
+}
