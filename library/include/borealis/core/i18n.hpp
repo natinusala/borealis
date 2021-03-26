5d8e98731a3c3decb338e08b6d21d133d0c717e5
EmreTech <50607143+EmreTech@users.noreply.github.com>
2021-03-25 21:23:45 -0700

    Added copyright

--- library/include/borealis/core/i18n.hpp
+++ library/include/borealis/core/i18n.hpp
@@ -1,5 +1,6 @@
 /*
     Copyright 2020-2021 natinusala
+    Copyright 2021 EmreTech
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
