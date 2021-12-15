bdcad36a9ae343587f390629ff53982b94cf1a02
EmreTech <50607143+EmreTech@users.noreply.github.com>
2021-12-14 20:06:00 -0800

    Added pretty-printing to i18n json to xml script

--- scripts/i18n-json-to-xml.py
+++ scripts/i18n-json-to-xml.py
@@ -1,6 +1,18 @@
 import json
 import xml.etree.ElementTree as ET
 
+# Function from https://stackoverflow.com/a/65808327
+def _pretty_print(current, parent=None, index=-1, depth=0):
+    for i, node in enumerate(current):
+        _pretty_print(node, current, i, depth + 1)
+    if parent is not None:
+        if index == 0:
+            parent.text = '\n' + ('\t' * depth)
+        else:
+            parent[index - 1].tail = '\n' + ('\t' * depth)
+        if index == len(parent) - 1:
+            current.tail = '\n' + ('\t' * (depth - 1))
+
 def processFile(data: dict, root: ET.Element):
     keys = data.keys()
 
@@ -20,6 +32,9 @@ with open(fileToConvert, "r") as json_f:
 
     # Loop through the entire JSON file
     processFile(data, root)
+
+    # Pretty-print
+    _pretty_print(root)
 
     tree = ET.ElementTree(root)
     tree.write(pathToSaveUnder)
